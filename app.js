1ea0cff83e62b5af0ff17ee497065845dd63262f
sabinaismailova <112122270+sabinaismailova@users.noreply.github.com>
2023-02-04 23:55:57 +0000

    trying to make add button work

--- app.js
+++ app.js
@@ -8,4 +8,16 @@ function addTask(){
     else
         document.getElementById("list-group").appendChild(li);
     document.getElementById("message-text").value = "";
+    var span = document.createElement("SPAN");
+                var txt = document.createTextNode("\u00D7");
+                span.className = "close";
+                span.appendChild(txt);
+                li.appendChild(span);
+
+                for(i=0;i<close.length;i++){
+                    close[i].onclick = function(){
+                        var div = this.parentElement;
+                        div.style.display = "none";
+                    }
+                }
 }
